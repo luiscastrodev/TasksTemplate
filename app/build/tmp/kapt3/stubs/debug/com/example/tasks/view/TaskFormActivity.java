c6a1c33e6c81f2e7326d49a0f88c6c2ba578c2a9
luis <luiscastrodev@gmail.com>
2020-11-22 22:50:48 -0300

    date picker tasks

--- app/build/tmp/kapt3/stubs/debug/com/example/tasks/view/TaskFormActivity.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/tasks/view/TaskFormActivity.java
@@ -2,9 +2,10 @@ package com.example.tasks.view;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u00012\u00020\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003J\b\u0010\u0006\u001a\u00020\u0007H\u0002J\b\u0010\b\u001a\u00020\u0007H\u0002J\u0010\u0010\t\u001a\u00020\u00072\u0006\u0010\n\u001a\u00020\u000bH\u0016J\u0012\u0010\f\u001a\u00020\u00072\b\u0010\r\u001a\u0004\u0018\u00010\u000eH\u0014R\u000e\u0010\u0004\u001a\u00020\u0005X\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000f"}, d2 = {"Lcom/example/tasks/view/TaskFormActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "Landroid/view/View$OnClickListener;", "()V", "mViewModel", "Lcom/example/tasks/viewmodel/RegisterViewModel;", "listeners", "", "observe", "onClick", "v", "Landroid/view/View;", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
-public final class TaskFormActivity extends androidx.appcompat.app.AppCompatActivity implements android.view.View.OnClickListener {
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000F\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0002\b\u0004\u0018\u00002\u00020\u00012\u00020\u00022\u00020\u0003B\u0005\u00a2\u0006\u0002\u0010\u0004J\b\u0010\t\u001a\u00020\nH\u0002J\b\u0010\u000b\u001a\u00020\nH\u0002J\u0010\u0010\f\u001a\u00020\n2\u0006\u0010\r\u001a\u00020\u000eH\u0016J\u0012\u0010\u000f\u001a\u00020\n2\b\u0010\u0010\u001a\u0004\u0018\u00010\u0011H\u0014J*\u0010\u0012\u001a\u00020\n2\b\u0010\u0013\u001a\u0004\u0018\u00010\u00142\u0006\u0010\u0015\u001a\u00020\u00162\u0006\u0010\u0017\u001a\u00020\u00162\u0006\u0010\u0018\u001a\u00020\u0016H\u0016J\b\u0010\u0019\u001a\u00020\nH\u0002R\u000e\u0010\u0005\u001a\u00020\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\bX\u0082.\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u001a"}, d2 = {"Lcom/example/tasks/view/TaskFormActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "Landroid/view/View$OnClickListener;", "Landroid/app/DatePickerDialog$OnDateSetListener;", "()V", "mDateFormat", "Ljava/text/SimpleDateFormat;", "mViewModel", "Lcom/example/tasks/viewmodel/RegisterViewModel;", "listeners", "", "observe", "onClick", "v", "Landroid/view/View;", "onCreate", "savedInstanceState", "Landroid/os/Bundle;", "onDateSet", "p0", "Landroid/widget/DatePicker;", "year", "", "month", "day", "showDataPicker", "app_debug"})
+public final class TaskFormActivity extends androidx.appcompat.app.AppCompatActivity implements android.view.View.OnClickListener, android.app.DatePickerDialog.OnDateSetListener {
     private com.example.tasks.viewmodel.RegisterViewModel mViewModel;
+    private final java.text.SimpleDateFormat mDateFormat = null;
     private java.util.HashMap _$_findViewCache;
     
     @java.lang.Override()
@@ -17,10 +18,18 @@ public final class TaskFormActivity extends androidx.appcompat.app.AppCompatActi
     android.view.View v) {
     }
     
+    private final void showDataPicker() {
+    }
+    
     private final void observe() {
     }
     
     private final void listeners() {
+    }
+    
+    @java.lang.Override()
+    public void onDateSet(@org.jetbrains.annotations.Nullable()
+    android.widget.DatePicker p0, int year, int month, int day) {
     }
     
     public TaskFormActivity() {
